36b5fa7c5dccaea812eab953895d865d163752ff
Riswan Noorhidayat <44827791+riswannh@users.noreply.github.com>
2020-08-04 22:20:37 +0800

    add custom dialog, but to small

--- app/src/main/java/com/example/myapplication/Menu.java
+++ app/src/main/java/com/example/myapplication/Menu.java
@@ -2,19 +2,26 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 public class Menu extends AppCompatActivity {
     Button btn;
+    Button btnDialog;
+    Dialog customDialog;
     Context ctx;
+    Button btnOk;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         ctx = this;
+        initFilter();
         btn = (Button) findViewById(R.id.btnRv);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,5 +29,29 @@ public class Menu extends AppCompatActivity {
                 MainActivity.startThisActivity(ctx);
             }
         });
+
+        btnDialog = (Button) findViewById(R.id.btnShowDialog);
+        btnDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                customDialog.show();
+            }
+        });
+
+
     }
+
+    private void initFilter() {
+        customDialog = new Dialog(Menu.this);
+        customDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        customDialog.setContentView(R.layout.filter_dialog);
+        btnOk = customDialog.findViewById(R.id.btnOk);
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                customDialog.dismiss();
+            }
+        });
+    }
+
 }
